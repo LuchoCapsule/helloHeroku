499ce007044c21bb1825f1f93611284d7a960c7f
Luis Armando <lagonzalez@xms.cl>
2024-09-24 14:49:43 -0300

    Project will be upload to heroku platform

---
+++ app.py
@@ -0,0 +1,12 @@
+# create a flask app
+from flask import Flask, render_template, request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hellow Heroku"
+
+if __name__ == '__main__':
+    app.run()
+
